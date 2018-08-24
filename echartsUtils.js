d03c21c4ccea497d78a543045aabebb3358eb593
G-shitou <43853022@qq.com>
2018-08-24 17:38:42 +0800

    配置优化,toolbox添加排序功能,待完善

--- echartsUtils.js
+++ echartsUtils.js
@@ -215,7 +215,7 @@ var myEcharts = {
                             pixelRatio: 2
                         },
                         magicType: {
-                            type: (obj.type && (obj.type == 'line')) ? [] : ['stack', 'tiled']
+                            type: ((obj.type == 'line') || (initData.kind.length === 1)) ? [] : ['stack', 'tiled']
                         },
                         dataView: {
                             title: '数据视图',
@@ -242,11 +242,43 @@ var myEcharts = {
                                 return table;
                             },
                             buttonColor: dataViewBgColor
+                        },
+                        mySort: {
+                            show: (initData.kind.length === 1) ? true : false,
+                            title: '排序',
+                            icon: 'path://M432.45,595.444c0,2.177-4.661,6.82-11.305,6.82c-6.475,0-11.306-4.567-11.306-6.82s4.852-6.812,11.306-6.812C427.841,588.632,432.452,593.191,432.45,595.444L432.45,595.444z M421.155,589.876c-3.009,0-5.448,2.495-5.448,5.572s2.439,5.572,5.448,5.572c3.01,0,5.449-2.495,5.449-5.572C426.604,592.371,424.165,589.876,421.155,589.876L421.155,589.876z M421.146,591.891c-1.916,0-3.47,1.589-3.47,3.549c0,1.959,1.554,3.548,3.47,3.548s3.469-1.589,3.469-3.548C424.614,593.479,423.062,591.891,421.146,591.891L421.146,591.891zM421.146,591.891',
+                            onclick: function(e){
+                                var data = initData.data[0].data,
+                                    name = initData.name;
+                                var sortName = name.map(function(value,index){
+                                    return {
+                                        index:data[index],
+                                        value:value
+                                    }
+                                }).sort(function(obj1,obj2){
+                                    return obj2.index - obj1.index;
+                                }).map(function(value,index){
+                                    return value.value;
+                                });
+                                var sortData = data.sort(function(value1,value2){
+                                    return value2 - value1; 
+                                });
+                                console.log(e);
+                                var myChart = echarts.init(document.getElementById(obj.echartsId));
+                                myChart.setOption({
+                                    xAxis:{
+                                        data:sortName
+                                    },
+                                    series:{
+                                        data:sortData
+                                    }
+                                });
+                            }
                         }
                     }
                 },
                 legend: {
-                    show: (obj.showLegend === false) ? obj.showLegend : true,
+                    show: (obj.showLegend === false || initData.kind.length === 1) ? false : true,
                     left: 'center',
                     top: 50,
                     data: type
